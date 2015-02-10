ce8f5550040fae19ab44214004afb7508c0a650c
Geoff Garside <geoff@geoffgarside.co.uk>
2015-02-10 07:50:02 +0000

    Remove httptest.Server test example, will use ResponseRecorder.

--- client_test.go
+++ client_test.go
@@ -10,36 +10,6 @@ import (
 	"testing"
 )
 
-func TestLearnHttpTestServer(t *testing.T) {
-	expected := "Hello, client"
-
-	url, _ := url.Parse("https://cas.host")
-	client := NewClient(&Options{
-		URL: url,
-	})
-
-	ts := httptest.NewServer(client.HandleFunc(func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintln(w, expected)
-	}))
-	defer ts.Close()
-
-	res, err := http.Get(ts.URL)
-	if err != nil {
-		t.Error(err)
-	}
-
-	body, err := ioutil.ReadAll(res.Body)
-	res.Body.Close()
-
-	if err != nil {
-		t.Error(err)
-	}
-
-	if expected != strings.Trim(string(body), "\n") {
-		t.Errorf("expected body to equal <%s>, got <%s>", expected, body)
-	}
-}
-
 func TestLearnHttpTestResponseRecorder(t *testing.T) {
 	expected := "Hello, client"
 
