7a2b8e77598bba0bd5dc214e94e98fc500bffb53
Geoff Garside <geoff@geoffgarside.co.uk>
2015-02-10 07:49:24 +0000

    Try httptest.ResponseRecorder

--- client_test.go
+++ client_test.go
@@ -10,7 +10,7 @@ import (
 	"testing"
 )
 
-func TestLearnHttpTest(t *testing.T) {
+func TestLearnHttpTestServer(t *testing.T) {
 	expected := "Hello, client"
 
 	url, _ := url.Parse("https://cas.host")
@@ -36,6 +36,33 @@ func TestLearnHttpTest(t *testing.T) {
 	}
 
 	if expected != strings.Trim(string(body), "\n") {
+		t.Errorf("expected body to equal <%s>, got <%s>", expected, body)
+	}
+}
+
+func TestLearnHttpTestResponseRecorder(t *testing.T) {
+	expected := "Hello, client"
+
+	url, _ := url.Parse("https://cas.host")
+	client := NewClient(&Options{
+		URL: url,
+	})
+
+	handler := client.HandleFunc(func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintln(w, expected)
+	})
+
+	req, err := http.NewRequest("GET", "http://test.host/", nil)
+	if err != nil {
+		t.Error(err)
+	}
+
+	w := httptest.NewRecorder()
+	handler.ServeHTTP(w, req)
+
+	body := w.Body.String()
+
+	if expected != strings.Trim(body, "\n") {
 		t.Errorf("expected body to equal <%s>, got <%s>", expected, body)
 	}
 }
