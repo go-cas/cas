13544b4934f2bacbefe417c164b4908cee72cc77
Kaleb Olson <kaleb.olson@gmail.com>
2017-05-19 11:28:00 -0600

    Add feature: remove global *http.request map in favor of request contexts This should make using cas with other muxers besides the default http.NewServerMux work better as many muxers create copies of the request which invalidates the map keys

--- http_helpers.go
+++ http_helpers.go
@@ -1,9 +1,17 @@
 package cas
 
 import (
+	"context"
 	"net/http"
 	"sync"
 	"time"
+)
+
+type key int
+
+const ( // emulating enums is actually pretty ugly in go.
+	clientKey key = iota
+	authenticationResponseKey
 )
 
 var (
@@ -14,18 +22,18 @@ var (
 
 // setClient associates a Client with a http.Request.
 func setClient(r *http.Request, c *Client) {
-	mutex.Lock()
-	defer mutex.Unlock()
-
-	clients[r] = c
+	ctx := context.WithValue(r.Context(), clientKey, c)
+	r2 := r.WithContext(ctx)
+	*r = *r2
 }
 
 // getClient retrieves the Client associated with the http.Request.
 func getClient(r *http.Request) *Client {
-	mutex.RLock()
-	defer mutex.RUnlock()
-
-	return clients[r]
+	if c := r.Context().Value(clientKey); c != nil {
+		return c.(*Client)
+	} else {
+		return nil // explicitly pass along the nil to caller -- conforms to previous impl
+	}
 }
 
 // RedirectToLogin allows CAS protected handlers to redirect a request
@@ -57,19 +65,19 @@ func RedirectToLogout(w http.ResponseWriter, r *http.Request) {
 // setAuthenticationResponse associates an AuthenticationResponse with
 // a http.Request.
 func setAuthenticationResponse(r *http.Request, a *AuthenticationResponse) {
-	mutex.Lock()
-	defer mutex.Unlock()
-
-	data[r] = a
+	ctx := context.WithValue(r.Context(), authenticationResponseKey, a)
+	r2 := r.WithContext(ctx)
+	*r = *r2
 }
 
 // getAuthenticationResponse retrieves the AuthenticationResponse associated
 // with a http.Request.
 func getAuthenticationResponse(r *http.Request) *AuthenticationResponse {
-	mutex.RLock()
-	defer mutex.RUnlock()
-
-	return data[r]
+	if a := r.Context().Value(authenticationResponseKey); a != nil {
+		return a.(*AuthenticationResponse)
+	} else {
+		return nil // explicitly pass along the nil to caller -- conforms to previous impl
+	}
 }
 
 // clear removes the Client and AuthenticationResponse associations of a http.Request.
